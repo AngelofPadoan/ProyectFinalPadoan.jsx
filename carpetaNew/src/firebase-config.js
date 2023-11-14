cb152172bddd0e839365bba26c059da23e2d8608
AngelofPadoan <134550505+AngelofPadoan@users.noreply.github.com>
2023-11-14 15:42:53 -0300

    initial commit

---
+++ carpetaNew/src/firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC2O3AXaap2ugwgTWAxOO7n95mBZGu4L28",
+    authDomain: "reactfirebaseproyect.firebaseapp.com",
+    projectId: "reactfirebaseproyect",
+    storageBucket: "reactfirebaseproyect.appspot.com",
+    messagingSenderId: "596670293206",
+    appId: "1:596670293206:web:48231acd6448876a3cd577"
+    measurementId: "G-32DL48G3V8",
+};
